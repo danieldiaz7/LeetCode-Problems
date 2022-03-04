7c17b268109c15b61c5a409437a0a0f8cd20125e
Daniel Diaz <96752650+danieldiaz7@users.noreply.github.com>
2022-03-04 15:24:06 -0500

    Update two_sum_solution.cpp

--- two_sum_solution.cpp
+++ two_sum_solution.cpp
@@ -1,6 +1,6 @@
 /*
 Daniel Diaz
-LeetCode Algorithms Problem - Two Sum
+LeetCode Problem - Two Sum
 
 - Description:
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
