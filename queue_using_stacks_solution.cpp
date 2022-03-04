ee9ddc2d36b697a85ff3f0b5a480bf36b005a547
Daniel Diaz <96752650+danieldiaz7@users.noreply.github.com>
2022-03-04 15:23:22 -0500

    Update queue_using_stacks_solution.cpp

--- queue_using_stacks_solution.cpp
+++ queue_using_stacks_solution.cpp
@@ -1,6 +1,6 @@
 /*
 Daniel Diaz
-LeetCode Algorithms Problem - Implement Queue using Stacks
+LeetCode Problem - Implement Queue using Stacks
 - Description:
 Implement a first in first out (FIFO) queue using only two stacks. 
 The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
